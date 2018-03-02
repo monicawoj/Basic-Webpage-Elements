e441e14162cb4feaf6f521d3b042348b47adf19e
Monica Wojciechowski <monica.wojciechowski@gmail.com>
2018-03-02 12:54:20 +0100

    Slider and Gallery

--- 1_Slider/js/app.js
+++ 1_Slider/js/app.js
@@ -1,3 +1,44 @@
 /**
  * Created by Jacek on 2015-12-16.
  */
+
+document.addEventListener('DOMContentLoaded', function() {
+    var previousButton = document.querySelector('#prevPicture');
+    var nextButton = document.querySelector('#nextPicture');
+    var sliderItems = document.querySelectorAll('.slider li');
+    var sliderIndex = 0;
+
+    //initialize first image
+    makeVisible(sliderIndex);
+
+
+    //move to previous
+    previousButton.addEventListener('click', function(event) {
+        sliderIndex -= 1;
+        //for 'previous' when first image
+        if (sliderIndex < 0) {
+            sliderIndex = sliderItems.length - 1;
+        }
+        makeVisible(sliderIndex);
+    });
+
+    //move to next
+    nextButton.addEventListener('click', function(event) {
+        sliderIndex += 1;
+        //for 'next' when last image visible
+        if (sliderIndex > sliderItems.length - 1) {
+            sliderIndex = 0;
+        }
+        makeVisible(sliderIndex);
+    });
+
+    //make list item visible
+    function makeVisible(index) {
+        var visibleItem = document.querySelector('li.visible');
+        if (visibleItem !== null) {
+            visibleItem.classList.remove('visible');
+        }
+        sliderItems[index].classList.add('visible');
+    }
+
+});
--- 2_Gallery/js/app.js
+++ 2_Gallery/js/app.js
@@ -2,9 +2,32 @@
  * Created by Jacek on 2015-12-16.
  */
 
-/*
- <div class="fullScreen">
-   <img src="./images/wrong.png">
-   <button class="close">Close</button>
- </div>
- */
+document.addEventListener('DOMContentLoaded', function() {
+    var galleryItems = document.querySelectorAll('.gallery li');
+    var body = document.querySelector('body');
+
+    galleryItems.forEach(function(element) {
+        element.addEventListener('click', function(event) {
+            var imgSrc = this.firstElementChild.getAttribute('src');
+
+            //append selected image to body
+            var fullScreenItem = document.createElement('div');
+            var newImg = document.createElement('img');
+            newImg.setAttribute('src',imgSrc);
+
+            var newButton = document.createElement('button');
+            newButton.classList.add('close');
+
+            fullScreenItem.appendChild(newImg);
+            fullScreenItem.appendChild(newButton);
+            body.appendChild(fullScreenItem);
+
+            //when new button clicked
+            newButton.addEventListener('click', function(event) {
+               body.removeChild(fullScreenItem);
+            });
+
+        });
+    });
+
+});
