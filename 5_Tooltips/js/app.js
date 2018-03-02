fdf457502fe7149827c22453bdbbd62783eb75ce
Monica Wojciechowski <monica.wojciechowski@gmail.com>
2018-03-02 15:24:56 +0100

    tooltip

--- 5_Tooltips/js/app.js
+++ 5_Tooltips/js/app.js
@@ -1,4 +1,25 @@
+document.addEventListener('DOMContentLoaded', function() {
 
+    var tooltips = document.querySelectorAll('.tooltip');
+    //console.log(tooltips);
+    [...tooltips].forEach(function(element) {
+
+        //add tooltip on mouseover
+        element.addEventListener('mouseover', function(event) {
+            var tooltipToAdd = document.createElement('span');
+            tooltipToAdd.classList.add('tooltipText');
+            tooltipToAdd.innerText = element.dataset.text;
+            element.appendChild(tooltipToAdd);
+        });
+
+        //hide tooltip on mouseout
+        element.addEventListener('mouseout', function(event) {
+            var tooltipToRemove = element.querySelector('span');
+            element.removeChild(tooltipToRemove);
+        });
+    });
+
+});
 
 
 /*
