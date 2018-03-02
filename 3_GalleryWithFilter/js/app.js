e78d2eb62462811b21c57a9461a193735b448adb
Monica Wojciechowski <monica.wojciechowski@gmail.com>
2018-03-02 14:35:48 +0100

    gallery with filter

--- 3_GalleryWithFilter/js/app.js
+++ 3_GalleryWithFilter/js/app.js
@@ -1,3 +1,35 @@
-/**
- * Created by Jacek on 2015-12-16.
- */
+document.addEventListener('DOMContentLoaded', function() {
+
+    var imgs = document.querySelectorAll('#gallery img');
+    var hideButton = document.querySelector('#hideButton');
+    var showButton = document.querySelector('#showButton');
+    var tagInput = document.querySelector('#tagInput');
+
+    hideButton.addEventListener('click', function(event) {
+        showOrHide('hide');
+    });
+
+    showButton.addEventListener('click', function(event) {
+        showOrHide('show');
+    });
+
+    function showOrHide(action) {
+        var tagInputText = tagInput.value;
+        //console.log(tagInputText);
+        imgs.forEach(function(element) {
+            var allTags = element.dataset.tag;
+            if (allTags.indexOf(tagInputText) !== -1) {
+                if (action == 'show') {
+                    element.classList.remove('invisible');
+                } else if (action == 'hide') {
+                    element.classList.add('invisible');
+                }
+            }
+            else {
+                console.log('No images with that tag, sorry!');
+            }
+        });
+        tagInput.value = '';
+    }
+
+});
